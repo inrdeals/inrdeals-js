0fbd5852b339afe0ff9c1acc140360ba003bc79a
roshni <roshni.espeed@gmail.com>
2020-01-02 19:12:25 +0530

    Js Snippet SubID Added

--- inrdeals.js
+++ inrdeals.js
@@ -2,19 +2,20 @@
 //  var store_name =  ["flipkart", "1mglabs", "agoda", "airtel", "amzer","beardo","bluestone", "bro4u", "cleartrip", "dailyobjects", "etihad", "expedia", "faasos", "firstcry", "flaberry", "floweraura", "freshmenu", "zotezo", "ebay", "amazon", "1mg","abof", "dailyobjects", "infibeam", "jabong", "voonik", "lenskart", "myntra", "paytm", "homeshop18", "shopclues", "tatacliq", "ajio", "aplava", "biba", "adidas", "coolwinks", "clovia", "chumbak", "dominos", "droom", "fabellay", "fabfurnish", "koovs", "netmeds", "zivame", "makemytrip", "zoomcar", "goair", "godaddy", "happilyunmarried", "hopscotch", "housejoy", "hpshopping", "igp", "industrybuying", "wonderchef", "kraftly", "vlccpersonalcare", "voylla", "treebohotels", "ticketgoose", "thyrocare", "thebodyshop", "littleapp", "swiggy", "mobikwik", "montecarlo", "myflowertree", "nearbuy", "nykaa", "pepperfry", "prettysecrets", "printland", "printvenue", "puma", "bigbasket", "fnp", "shoppersstop", "bookmyshow", "bigrock", "pizzahut", "titan", "foodpanda", "aliexpress", "healthkart", "ixigo", "nnnow", "lurap", "stalkbuylove", "jio", "metroshoes", "fabindia", "bata", "wforwoman", "mrvoonik", "croma", "nike", "medlife", "grofers", "yatra", "lifestylestores", "maxfashion", "gadgetsnow", "bewakoof", "jetairways", "paytmmall", "pvrcinemas", "footprint360", "limeroad", "cheapticket", "reliancetrends", "urbanclap", "dunkinindia", "moglix", "printoctopus", "myvishal", "kidzee", "shopping.rediff", "shein", "themeforest", "codecanyon", "videohive", "audiojungle", "graphicriver", "photodune", "3docean", "jockeyindia", "indianterrain", "clarks"];
 var store_name = [];
 $(document).ready(function(){
+    var subID1 = (subID1 === undefined) ? null : subID1;
 	console.log('Inrdeals Username : ' +subID);
 	  $.get("https://cdn.jsdelivr.net/gh/inrdeals/inrdeals-js@latest/data.json", function(data, status){
         var store_name = data.keywords;
         console.log(store_name);
-        checkURL(subID, store_name);
+        checkURL(subID, store_name, subID1);
     });
    
 });
 
-function checkURL(subID, store_name)
+function checkURL(subID, store_name, subID1=null)
 {
 	$('body').find('a').each(function(){
-		var param = "http://inr.deals/track?id="+subID+"&url=";
+		var param = "https://inr.deals/track?id="+subID+"&subid="+subID1+"&url=";
 		var $this = $(this);
 		var _href = $this.attr('href');
 		var host = new URL(_href);
