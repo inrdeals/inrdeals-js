040cad259cb3e023db6fedd61d3df6a63a83cdea
roshni1 <gupta.roshnigupta@gmail.com>
2022-02-17 18:13:35 +0530

    Js Stores Updated

--- inrdeals.js
+++ inrdeals.js
@@ -1,13 +1,11 @@
-// fill store names in store_name array
-//  var store_name =  ["flipkart", "1mglabs", "agoda", "airtel", "amzer","beardo","bluestone", "bro4u", "cleartrip", "dailyobjects", "etihad", "expedia", "faasos", "firstcry", "flaberry", "floweraura", "freshmenu", "zotezo", "ebay", "amazon", "1mg","abof", "dailyobjects", "infibeam", "jabong", "voonik", "lenskart", "myntra", "paytm", "homeshop18", "shopclues", "tatacliq", "ajio", "aplava", "biba", "adidas", "coolwinks", "clovia", "chumbak", "dominos", "droom", "fabellay", "fabfurnish", "koovs", "netmeds", "zivame", "makemytrip", "zoomcar", "goair", "godaddy", "happilyunmarried", "hopscotch", "housejoy", "hpshopping", "igp", "industrybuying", "wonderchef", "kraftly", "vlccpersonalcare", "voylla", "treebohotels", "ticketgoose", "thyrocare", "thebodyshop", "littleapp", "swiggy", "mobikwik", "montecarlo", "myflowertree", "nearbuy", "nykaa", "pepperfry", "prettysecrets", "printland", "printvenue", "puma", "bigbasket", "fnp", "shoppersstop", "bookmyshow", "bigrock", "pizzahut", "titan", "foodpanda", "aliexpress", "healthkart", "ixigo", "nnnow", "lurap", "stalkbuylove", "jio", "metroshoes", "fabindia", "bata", "wforwoman", "mrvoonik", "croma", "nike", "medlife", "grofers", "yatra", "lifestylestores", "maxfashion", "gadgetsnow", "bewakoof", "jetairways", "paytmmall", "pvrcinemas", "footprint360", "limeroad", "cheapticket", "reliancetrends", "urbanclap", "dunkinindia", "moglix", "printoctopus", "myvishal", "kidzee", "shopping.rediff", "shein", "themeforest", "codecanyon", "videohive", "audiojungle", "graphicriver", "photodune", "3docean", "jockeyindia", "indianterrain", "clarks"];
 var $ = jQuery;
 var store_name = [];
 $(document).ready(function(){
-    var aff_subid = (aff_sub === undefined) ? null : aff_sub;
+    var aff_subID = (typeof aff_sub === 'undefined') ? null : aff_sub;
 	console.log('Inrdeals Username : ' +subID);
 	  $.get("https://cdn.jsdelivr.net/gh/inrdeals/inrdeals-js@latest/data.json", function(data, status){
         store_name = data.keywords;
-        checkURL(subID, store_name, aff_subid);
+        checkURL(subID, store_name, aff_subID);
     });
 });
 
